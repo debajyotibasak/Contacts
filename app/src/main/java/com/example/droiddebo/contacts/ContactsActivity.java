98daf68da7bd4973b78718d614ece3e0aa964c28
debo1994 <d.basak.db@gmail.com>
2017-05-12 03:50:43 +0530

    "No Internet Connection" if no Internet connection

--- app/src/main/java/com/example/droiddebo/contacts/ContactsActivity.java
+++ app/src/main/java/com/example/droiddebo/contacts/ContactsActivity.java
@@ -1,7 +1,10 @@
 package com.example.droiddebo.contacts;
 
 import android.app.LoaderManager;
+import android.content.Context;
 import android.content.Loader;
+import android.net.ConnectivityManager;
+import android.net.NetworkInfo;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
 import android.view.View;
@@ -47,16 +50,35 @@ public class ContactsActivity extends AppCompatActivity implements LoaderManager
         /*ContactsAsyncTask task = new ContactsAsyncTask();
           task.execute(REQUEST_URL);*/
 
-        // Get a reference to the LoaderManager, in order to interact with loaders.
-        LoaderManager loaderManager = getLoaderManager();
-
-        // Initialize the loader. Pass in the int ID constant defined above and pass in null for
-        // the bundle. Pass in this activity for the LoaderCallbacks parameter (which is valid
-        // because this activity implements the LoaderCallbacks interface).
-        loaderManager.initLoader(CONTACTS_LOADER_ID, null, this);
-
         mEmptyStateTextView = (TextView) findViewById(R.id.empty_view);
         contactListView.setEmptyView(mEmptyStateTextView);
+
+        // Get a reference to the ConnectivityManager to check state of network connectivity
+        ConnectivityManager connMgr = (ConnectivityManager) getSystemService(Context.CONNECTIVITY_SERVICE);
+
+        // Get details on the currently active default data network
+        NetworkInfo networkInfo = connMgr.getActiveNetworkInfo();
+
+        // If there is a network connection, fetch data
+        if (networkInfo != null && networkInfo.isConnected()) {
+            // Get a reference to the LoaderManager, in order to interact with loaders.
+            LoaderManager loaderManager = getLoaderManager();
+
+            // Initialize the loader. Pass in the int ID constant defined above and pass in null for
+            // the bundle. Pass in this activity for the LoaderCallbacks parameter (which is valid
+            // because this activity implements the LoaderCallbacks interface).
+            loaderManager.initLoader(CONTACTS_LOADER_ID, null, this);
+        } else {
+            // Otherwise, display error
+            // First, hide loading indicator so error message will be visible
+            View loadingIndicator = findViewById(R.id.loading_indicator);
+            loadingIndicator.setVisibility(View.GONE);
+
+            // Update empty state with no connection error message
+            mEmptyStateTextView.setText(R.string.no_internet_connection);
+        }
+
+
     }
 
     @Override
